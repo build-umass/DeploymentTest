9e2e7320fb1fe6e9f18ae8105e5160b2a2c365cf
Ian Birle <IBirle@umass.edu>
2020-02-25 23:36:59 -0500

    Added deployment

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ const port = 8080
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/test', (req, res) => res.send('test'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
